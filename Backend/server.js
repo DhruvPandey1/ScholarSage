76a8d4cc0f4dd4487ca55f6542b48e1fc12b2071
DhruvPandey1 <115931661+DhruvPandey1@users.noreply.github.com>
2025-07-12 11:38:11 +0530

    Ratelimiting is implemented

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const ratelimit=require('express-rate-limit');
 const db= require('./config/db');
 const morgan = require('morgan');
 const researchRoutes = require('./routes/research');
@@ -14,7 +15,22 @@ const app = express();
 const port = process.env.PORT || 3001;
 //Enable CORS
 app.use(cors());
-//Security middleware
+
+let windowMs = parseInt(process.env.RATE_LIMIT_WINDOW_MS) || 900000;
+let max = parseInt(process.env.RATE_LIMIT_MAX_REQUESTS) || 100;
+if (process.env.NODE_ENV === 'test') {
+  windowMs = 2000; // 2 seconds
+  max = 3;         // 3 requests
+}
+// Rate limiting
+const limiter = ratelimit({
+  windowMs: windowMs,
+  max: max,
+  message: 'Too many requests, please try again later.'
+});
+app.use(limiter);
+
+//Security 
 app.use(helmet());
 //Body parser middleware
 app.use(express.json());
@@ -29,8 +45,12 @@ app.use('/api/auth', authRoutes);
 app.get('/api/health', (req, res) => {
   res.status(200).json({ status: 'OK', message: 'Server is running' });
 });
-
+app.get('/test',(req,res)=>{
+  res.status(200).send('OK')
+})
 //App is listening on the specified port8
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-});
+})
+
+module.exports=app;
